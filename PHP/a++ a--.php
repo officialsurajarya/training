848149d5fe09cefd7f6413b23decf8164405e3f2
Suraj Arya <139065845+officialsurajarya@users.noreply.github.com>
2024-09-04 12:07:15 +0530

    Add files via upload

---
+++ PHP/a++ a--.php
@@ -0,0 +1,21 @@
+<?php
+#post-increment
+$a=5;
+++$a; 
+echo $a;
+
+#pre-increment
+$b=5;
+$b++;
+echo " ".$a;
+
+#post-decrement
+$a=5;
+--$a; 
+echo" ".$a;
+
+#pre-decrement
+$b=5;
+$b--;
+echo " ".$a;
+?>
