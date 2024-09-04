c84bc11c0d95994b049861b8f8ffc5a2dfbb4e31
Suraj Arya <139065845+officialsurajarya@users.noreply.github.com>
2024-09-04 11:40:45 +0530

    Add files via upload

---
+++ PHP/+ve or -ve.php
@@ -0,0 +1,9 @@
+<?php
+  $num=-5;
+  if($num > 0){
+    echo"Positive Number";
+  }  
+  else {
+    echo "Negative Number";
+  }
+?>
---
+++ PHP/withoutusing3var.php
@@ -0,0 +1,15 @@
+<?php
+
+$a=15;
+$b=25;
+
+$a=$a+$b;
+$b=$a-$b;
+$a=$a-$b;
+echo "a= ";
+echo $a;
+echo " ";
+echo "b= ";
+echo $b;
+
+?>
