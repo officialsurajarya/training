5ed3f131e2b6be6428017d704f2e9beca1b96bae
Suraj Arya <139065845+officialsurajarya@users.noreply.github.com>
2024-09-07 11:53:04 +0530

    Create Switch case Statment.php

---
+++ PHP/Switch case Statment.php
@@ -0,0 +1,38 @@
+<?php
+    $day=5;
+    switch($day){
+        
+        case 1:
+        echo "Today is Sunday";
+        break;
+
+        case 2:
+        echo "Today is Monday";
+        break;
+
+        case 3:
+        echo "Today is Tuesday ";
+        break;
+
+        case 4:
+        echo "Today is Wednesday";
+        break;
+
+        case 5:
+        echo "Today is Thrusday";
+        break;
+
+        case 6:
+        echo "Today is Friday";
+        break;
+
+        case 7:
+        echo "Today is Saturday";
+        break;
+        
+        default:
+        echo "Invalid Choice";
+        break;
+    }
+
+?>
