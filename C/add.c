4d57acc87e6d2496009e3ce035c65cbb844be00f
Suraj Arya <139065845+officialsurajarya@users.noreply.github.com>
2024-10-22 21:39:11 +0530

    Create add.c

---
+++ C/add.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	//Add to numbers
+	int a,b;
+	printf("Enter a: ");
+	scanf("%d",&a);
+	
+	printf("Enter b: ");
+	scanf("%d",&b);
+	
+	int sum = a + b ;
+	printf("Sum is : %d",sum);
+	
+	return 0;
+}
